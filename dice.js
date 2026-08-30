7a0283a1ac8facf94aae1cd95865a31f341b457e
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-08-31 00:00:48 +0530

    652

--- dice.js
+++ dice.js
@@ -1,11 +1,9 @@
 var crypto = require("crypto");
- 
 function rollDice() {
   // crypto.randomInt(min, max) gives a random number, max is not included
   var value = crypto.randomInt(1, 7);
   return value;
 }
- 
 // Roll the dice 5 times
 for (var i = 1; i <= 5; i++) {
   var result = rollDice();
