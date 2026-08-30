1e131644b286581b977bf81cd7172ffae1f6cfe6
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-08-31 00:06:46 +0530

    56

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var isEven = require("./modules/isEven");
 var logger = require("./modules/logger");
+
 logger("Starting app.js");
+
 for (var i = 1; i <= 5; i++) {
   if (isEven(i)) {
     console.log(i + " is Even");
@@ -8,5 +10,6 @@ for (var i = 1; i <= 5; i++) {
     console.log(i + " is Odd");
   }
 }
+
 logger("Finished checking numbers");
  
