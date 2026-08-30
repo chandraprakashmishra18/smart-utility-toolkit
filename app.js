d5718b53ec3c93a4f0005dbc968e45b9f1c6790e
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-08-30 23:59:53 +0530

    4

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var isEven = require("./modules/isEven");
 var logger = require("./modules/logger");
- 
 logger("Starting app.js");
- 
 for (var i = 1; i <= 5; i++) {
   if (isEven(i)) {
     console.log(i + " is Even");
