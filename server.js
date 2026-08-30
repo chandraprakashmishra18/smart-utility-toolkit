fc022508b4c9611188552847284ff96a40e48ff0
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-08-31 00:01:59 +0530

    5

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 var http = require("http");
 var server = http.createServer(function (req, res) {
   console.log("Request received for:", req.url);
- 
   if (req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Welcome to Node Server");
